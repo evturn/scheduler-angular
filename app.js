b423a25c91d5ccf0a8741e101251ccdaf4d8bbfb
Evan Turner <evturn@gmail.com>
2015-02-23 18:36:02 -0500

    .reset function written to set the default data

--- app.js
+++ app.js
@@ -5,4 +5,36 @@ angular.module('scheduleApp', ['firebase'])
 	var ref = new Firebase("https://scheduler-angular.firebaseio.com/");  
   var fb = $firebase(ref);
 
+  $scope.reset = function() {
+  	fb.$set({
+  		monday: {
+  			name: 'Monday',
+  			slots: {
+  				0900: {
+  					time: '9:00am',
+  					booked: false
+  				},
+  				0110: {
+  					time: '11:00am',
+  					booked: false
+  				}
+  			}
+  		},
+  		tuesday: {
+  			name: 'Tuesday',
+  			slots: {
+  				09:00: {
+  					time: '9:00am',
+  					booked: false
+  				},
+  				0110: {
+  					time: '11:00am',
+  					booked: false
+  				}
+  			}
+  		}
+  	});
+
+  };
+
 });
