656b8a0ab47baf8a24aeb6c11def75937d706642
Evan Turner <evturn@gmail.com>
2015-02-23 18:46:31 -0500

    JSON object from firebase using three-way binding

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ angular.module('scheduleApp', ['firebase'])
 
 .controller('mainController', function($scope, $firebase) {
 
-	var ref = new Firebase("https://scheduler-angular.firebaseio.com/");  
+	var ref = new Firebase("https://scheduler-angular.firebaseio.com/days");  
   var fb = $firebase(ref);
 
   var syncObject = fb.$asObject();
+  sycnObject.$bindTo($scope, 'days');
 
   $scope.reset = function() {
   	fb.$set({
