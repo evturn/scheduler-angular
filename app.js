64a6e90cc39e51646b5cd82af9a5bb0670ae93ab
Evan Turner <evturn@gmail.com>
2015-02-23 19:02:38 -0500

    data now coming from firebase

--- app.js
+++ app.js
@@ -6,38 +6,8 @@ angular.module('scheduleApp', ['firebase'])
   var fb = $firebase(ref);
 
   var syncObject = fb.$asObject();
-  sycnObject.$bindTo($scope, 'days');
 
-  $scope.reset = function() {
-  	fb.$set({
-  		monday: {
-  			name: 'Monday',
-  			slots: {
-  				0900: {
-  					time: '9:00am',
-  					booked: false
-  				},
-  				0110: {
-  					time: '11:00am',
-  					booked: false
-  				}
-  			}
-  		},
-  		tuesday: {
-  			name: 'Tuesday',
-  			slots: {
-  				0900: {
-  					time: '9:00am',
-  					booked: false
-  				},
-  				0110: {
-  					time: '11:00am',
-  					booked: false
-  				}
-  			}
-  		}
-  	});
+  syncObject.$bindTo($scope, 'days');
 
-  };
-
+ 
 });
